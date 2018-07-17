b2d1d1b1834ac0097c62b84fc93422009257fa5c
JoesMac <joesmac@JoesMacs-Mac.local>
2018-07-17 15:01:47 -0700

    actions and outlets

--- Simon Says/ViewController.swift
+++ Simon Says/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorButtons: [CircularButton]!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet var playerLabels: [UILabel]!
+    @IBOutlet var scoreLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func colorButtonHandler(_ sender: CircularButton) {
+        
     }
-
-
+    
+    @IBAction func actionButtonHandler(_ sender: UIButton) {
+        
+    }
+    
 }
 
